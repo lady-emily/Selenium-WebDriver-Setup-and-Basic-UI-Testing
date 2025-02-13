35ad1f25589cc180408d941cf8e6f91ae0148a94
ladyemilyy <equarsh33@gmail.com>
2025-02-13 11:23:05 +0000

    simple class to test Chrome WebDriver connectivity

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ChromeTest {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "D:\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        System.out.println("Opened Google!");
-        driver.quit();
+        WebDriver driver = new ChromeDriver(); //initialise chrome
+        driver.get("https://www.google.com"); //load Google
+        System.out.println("Opened Google!"); //test for connection
+        driver.quit(); //close browser
     }
 }
